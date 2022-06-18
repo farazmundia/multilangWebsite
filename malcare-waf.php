374f56f71fb31698e6d0160e26a01f7df401813f
Faraz Mundia <farazmuhammad1995@gmail.com>
2022-06-18 19:00:34 +0500

    complete website

---
+++ malcare-waf.php
@@ -0,0 +1,9 @@
+<?php
+// Please validate auto_prepend_file setting before removing this file
+
+if (file_exists('/mnt/data/home/682620.cloudwaysapps.com/wgyyarwhra/public_html/wp-content/plugins/malcare-security/protect/prepend/ignitor.php')) {
+	define("MCDATAPATH", '/mnt/data/home/682620.cloudwaysapps.com/wgyyarwhra/public_html/wp-content/mc_data/');
+	define("MCCONFKEY", '13a4e89f4b099ce708f013127b723e68');
+	include_once('/mnt/data/home/682620.cloudwaysapps.com/wgyyarwhra/public_html/wp-content/plugins/malcare-security/protect/prepend/ignitor.php');
+}
+?>
